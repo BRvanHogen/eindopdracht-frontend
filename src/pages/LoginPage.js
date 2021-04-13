f85006100f68b108cfc4bd28dd35a8f57b7a0d49
BRvanHogen <brvanhogen@hotmail.nl>
2021-04-13 16:39:08 +0200

    React-hook-form geinstalleerd en ook alvast een sign up pagina aangemaakt.

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,20 +1,60 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 
 function LoginPage() {
+    const { handleSubmit, register, errors } = useForm();
+
+    function onFormSubmit(data) {
+        // data.preventDefault();
+        console.log(data);
+    }
+
     return (
-        <>
+        <form onSubmit={handleSubmit(onFormSubmit)}>
         <p>hier inloggen</p>
-            <label>
-        <input type="text" placeholder="gebruikersnaam"/>
+            <label htmlFor="username">
+        <input
+            type="text"
+            placeholder="gebruikersnaam"
+            name="username"
+            ref={register(
+                {
+                    required: {
+                    value: true,
+                    message: 'voer gebruikersnaam in',
+
+                    },
+                }
+                )}
+        />
             </label>
-            <label>
-                <input type="text" placeholder="wachtwoord"/>
+            {errors.username && <p>{errors.username.message}</p>}
+
+
+            <label htmlFor="password">
+                <input
+                    type="text"
+                    placeholder="wachtwoord"
+                    name="password"
+                    ref={register(
+                        {
+                            required: {
+                                value: true,
+                                message: 'voer wachtwoord in',
+                            }
+                        }
+                        )}
+                />
             </label>
-            <button type="button">log in</button>
-        </>
+            {errors.password && <p>{errors.password.message}</p>}
+
+
+            <button type="submit">
+                log in
+            </button>
+        </form>
     )
 }
-
 
 export default LoginPage;
