5dc67c42bdb4b018df020b15a8db55ee01652bcd
BRvanHogen <brvanhogen@hotmail.nl>
2021-05-04 13:07:42 +0200

    //project parameter verwijderd uit functie. Nu geen foutmelding 'can not read property of undefined'

--- src/components/delete-project/DeleteProject.js
+++ src/components/delete-project/DeleteProject.js
@@ -6,12 +6,14 @@ import {ProjectContext} from "../../context/ProjectContext";
 //STRATEGIE: we gaan het project wissen welke in de ProjectContext staat
 // ik heb geprobeerd met zowel name als project als variabele. Beiden geven andere foutmeldingen
 function DeleteProject() {
-    const { project } = useContext(ProjectContext);
+    const { project, exit } = useContext(ProjectContext);
 
-    async function DeleteThisProject(project) {
+    //project parameter verwijderd uit functie. Nu geen foutmelding 'can not read property of undefined'
+    async function DeleteThisProject() {
         try {
             console.log(project.name);
             const response = await axios.delete(`https://localhost:8444/projects/${project.name}`)
+            exit(project.name);
 
         } catch (e) {
             console.error(e);
