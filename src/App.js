9e229d531435f4cef14992287e68c25da50ca483
BRvanHogen <brvanhogen@hotmail.nl>
2021-05-25 15:45:08 +0200

    Routing beveiligd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './App.css';
-import {Switch, Route,} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import LoginPage from "./pages/login-page/LoginPage";
 import Dashboard from "./pages/profile/Dashboard";
 import BandDashboard from "./pages/BandDashboard";
@@ -10,13 +10,28 @@ import HomePage from "./pages/HomePage";
 import ProjectsPage from "./pages/projects-page/ProjectsPage";
 import Sidebar from "./components/sidebar/Sidebar";
 import TestPage from "./pages/test-page/TestPage";
+import {AuthContext} from "./context/AuthContext";
+
+function PrivateRoute({children, ...rest}) {
+    const {user} = useContext(AuthContext);
+
+    return (
+        <Route {...rest}>
+            {user ? children : <Redirect to="/sign-in"/>}
+        </Route>
+    )
+}
+
 
 
 function App() {
+    const {user} = useContext(AuthContext);
+
     return (
         <>
             <TopMenu/>
             <Switch>
+
                 <Route exact path="/">
                     <HomePage/>
                 </Route>
@@ -29,23 +44,29 @@ function App() {
                     <LoginPage/>
                 </Route>
 
-                <Route path="/profile">
+                {/*user={user} dit hoefde niet eens bij de properties van PrivateRoute? */}
+                <PrivateRoute exact path="/profile">
                     <Dashboard/>
-                </Route>
-                <Route path="/band-dashboard">
+                </PrivateRoute>
+
+                <PrivateRoute exact path="/band-dashboard">
                     <BandDashboard
                         title="Deze Song"
                     />
-                </Route>
-                <Route path="/projects">
+                </PrivateRoute>
+
+                <PrivateRoute exact path="/projects">
                     <ProjectsPage/>
-                </Route>
+                </PrivateRoute>
+
                 <Route path="/sidebar">
                     <Sidebar/>
                 </Route>
+
                 <Route path="/test-page">
                     <TestPage/>
                 </Route>
+
             </Switch>
 </>
     );
--- src/components/top-menu/TopMenu.js
+++ src/components/top-menu/TopMenu.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './top-menu.module.css';
+import {AuthContext} from "../../context/AuthContext";
 
 function TopMenu() {
+    const {user, logout} = useContext(AuthContext);
+
     return (
         <div className={styles['top-menu']}>
         <ul className={styles.ul}>
-            <li><NavLink to="/profile" className={styles.link} activeClassName={styles['active-link']}>dashboard</NavLink></li>
-            <li><NavLink to="/band-dashboard" activeClassName={styles['active-link']}>band dashboard</NavLink></li>
-            <li><NavLink to="/projects" activeClassName={styles['active-link']}>projects</NavLink></li>
-            <li><NavLink to="/sign-in" activeClassName={styles['active-link']}>log in</NavLink></li>
-            <li><NavLink to="/sign-up" activeClassName={styles['active-link']}>sign up</NavLink></li>
-        </ul>
+            {user !== null ?
+                <>
+                    <li><NavLink to="/profile" className={styles.link}
+                                 activeClassName={styles['active-link']}>dashboard</NavLink></li>
+                    <li><NavLink to="/band-dashboard" activeClassName={styles['active-link']}>band dashboard</NavLink>
+                    </li>
+                    <li><NavLink to="/projects" activeClassName={styles['active-link']}>projects</NavLink></li>
+                    <li><NavLink to="/sign-in" activeClassName={styles['active-link']} onClick={logout}>log out</NavLink> </li>
+                </>
+                :
+                <>
+                    <li><NavLink to="/sign-in" activeClassName={styles['active-link']}>log in</NavLink></li>
+                    <li><NavLink to="/sign-up" activeClassName={styles['active-link']}>sign up</NavLink></li>
+                </>
+            }
+            </ul>
         </div>
     )
 }
